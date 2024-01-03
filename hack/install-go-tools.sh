08cfeb2c5f6364f9f40ed7154528a230413b77f6
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2024-01-03 19:20:52 +0100

    fix: install po-lint properly

--- hack/install-go-tools.sh
+++ hack/install-go-tools.sh
@@ -9,12 +9,10 @@ fi
 
 cd "$(dirname $BASH_SOURCE)/.."
 
-go install -mod=readonly github.com/bwplotka/bingo@latest
-
 # The following PR has been merged in bingo:
 # https://github.com/bwplotka/bingo/pull/142
 # Once the new bingo version is released, make sure to append '-t 0' in below commands
 unset GOFLAGS
 bingo get -v -l github.com/prometheus/prometheus/cmd/promtool@v0.48.0
 bingo get -v -l github.com/cloudflare/pint/cmd/pint@v0.44.1
-go install -mod=readonly github.com/prometheus-operator/prometheus-operator/cmd/po-lint
+go install github.com/prometheus-operator/prometheus-operator/cmd/po-lint
